be23260685be879db1cc936bda532c2ff2788382
Horacio Correa <chdk2011@gmail.com>
2025-10-22 18:13:20 -0300

    3rd attempt

--- src/game/server/gamerules.h
+++ src/game/server/gamerules.h
@@ -216,8 +216,6 @@ public:
 	virtual int IPointsForKill(CBasePlayer *pAttacker, CBasePlayer *pKilled);
 	virtual void PlayerKilled(CBasePlayer *pVictim, entvars_t *pKiller, entvars_t *pInflictor);
 	virtual void DeathNotice(CBasePlayer *pVictim, entvars_t *pKiller, entvars_t *pInflictor);
-	virtual bool AreAssistsEnabled() const override;
-	virtual bool AreAssistsEnabled() const override;
 
 	// Weapon retrieval
 	virtual void PlayerGotWeapon(CBasePlayer *pPlayer, CBasePlayerItem *pWeapon);
@@ -313,6 +311,7 @@ public:
 	virtual int IPointsForKill(CBasePlayer *pAttacker, CBasePlayer *pKilled);
 	virtual void PlayerKilled(CBasePlayer *pVictim, entvars_t *pKiller, entvars_t *pInflictor);
 	virtual void DeathNotice(CBasePlayer *pVictim, entvars_t *pKiller, entvars_t *pInflictor);
+	virtual bool AreAssistsEnabled() const override;
 
 	// Weapon retrieval
 	virtual void PlayerGotWeapon(CBasePlayer *pPlayer, CBasePlayerItem *pWeapon);
